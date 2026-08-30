e6c08ac885b41cbcf8f1635ab09cfbf3721f251b
Advaith Binu <71909050+advaithbinu11@users.noreply.github.com>
2026-08-30 15:01:42 -0500

    Add binary addition function to Solution class

---
+++ day1.py
@@ -0,0 +1,42 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        freqdict = {}
+        max = nums[0]
+        for i in range(0, len(nums)):
+            if(freqdict.get(nums[i], None) == None):
+                freqdict[nums[i]] = 1
+            else:
+                freqdict[nums[i]] = freqdict[nums[i]] + 1
+                if(freqdict[max]<freqdict[nums[i]]):
+                    max=nums[i]
+        return max
+
+  class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        if(len(a)<len(b)):
+            c = a
+            a = b
+            b = c
+        s = ""
+        store = 0
+        bi = 0
+        for i in range(0, len(a)):
+            ai = int(a[len(a)-i-1])
+            if(i<len(b)):
+                bi = int(b[len(b)-i-1])
+            sum = ai + bi + store
+            if(sum % 2 == 0):
+                s = str(0)+s
+            else:
+                s = str(1)+s
+            if(sum>=2):
+                store = 1
+            else:
+                store = 0
+            bi = 0
+            ai = 0
+        if(store == 1):
+            s = str(1)+s
+        return s
+
+            
