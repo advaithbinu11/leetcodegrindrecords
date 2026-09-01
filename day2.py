b894f67cb01750feff248238cc7654e56ebf26b8
Advaith Binu <71909050+advaithbinu11@users.noreply.github.com>
2026-08-31 22:49:40 -0500

    Add KMP algorithm implementation in strStr method
    Implemented KMP algorithm for substring search in strStr method.

--- day2.py
+++ day2.py
@@ -18,3 +18,31 @@ class Solution:
                     return aInd-len(needle)
             aInd+=1
         return -1
+
+# Failed KMP Attempt
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        j = 0
+        parr = [0] * len(needle)
+        for i in range(1, len(needle)):
+            if(needle[i] == needle[j]):
+                parr[i] = parr[i-1]+1
+                j+=1
+            else:
+                if j!= 0:
+                    j = parr[j-1]
+                else:
+                    parr[i] = 0
+                    i+=1
+        j=0
+        print(parr)
+        for i in range(0, len(haystack)):
+            if(haystack[i] == needle[j]):
+                print("Hit " + str(i)+" "+str(j))
+                j+=1
+                if(j == len(needle)):
+                    return i-(len(needle)-1)
+            elif (j != 0):
+                print(parr[j-1])
+                j = parr[j-1]
+        return -1
