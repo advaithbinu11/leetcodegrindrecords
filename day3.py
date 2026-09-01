f4b80f8bf9d96de25dc27c718db33906694cb55f
Advaith Binu <71909050+advaithbinu11@users.noreply.github.com>
2026-09-01 09:38:51 -0500

    Implement isSubsequence method in Solution class
    Added a new method 'isSubsequence' to the Solution class to check if string 's' is a subsequence of string 't'.

--- day3.py
+++ day3.py
@@ -16,3 +16,15 @@ class Solution:
             else:
                 return False
         return True
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        j = 0
+        if (len(s)==0):
+            return True
+        for i in range(0, len(t)):
+            if(t[i] == s[j]):
+                j+=1
+                if(j == len(s)):
+                    return True
+        return False
+        
