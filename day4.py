3dffddf8be0ec415cf3a51bcf0386637b52e04b7
Advaith Binu <71909050+advaithbinu11@users.noreply.github.com>
2026-09-02 22:46:10 -0500

    Add canConstruct method to Solution class

--- day4.py
+++ day4.py
@@ -3,3 +3,18 @@ class Solution:
         arr = s.split()
         return len(arr[len(arr)-1])
         
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        dict = {}
+        for i in range(0, len(magazine)):
+            if dict.get(magazine[i], None) == None:
+                dict[magazine[i]] = 1
+            else:
+                dict[magazine[i]] += 1
+        for j in range(0, len(ransomNote)):
+            if dict.get(ransomNote[j], None) == None:
+                return False
+            dict[ransomNote[j]] -= 1
+            if dict[ransomNote[j]] == -1:
+                return False
+        return True
