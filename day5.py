9707292c32025939bea23d0370a8bdaa4659b0ea
Advaith Binu <71909050+advaithbinu11@users.noreply.github.com>
2026-09-03 12:47:35 -0500

    Implement integer palindrome check method
    Added a new method to check if an integer is a palindrome without converting it to a string.

--- day5.py
+++ day5.py
@@ -11,3 +11,16 @@ class Solution:
             i+=1
             j-=1
         return True
+
+#No string solution
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if(x<0 or (x!=0 and x%10==0)):
+            return False
+        rev = 0
+        while (x>rev):
+            digit = x%10
+            rev = 10*rev + digit
+            x//=10
+
+        return x == rev or rev//10 == x
