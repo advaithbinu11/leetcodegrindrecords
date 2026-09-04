47fe07c9005d8bf26b718a6c71c0bd9fe8eaf930
Advaith Binu <71909050+advaithbinu11@users.noreply.github.com>
2026-09-04 09:39:05 -0500

    Implement summaryRanges method in Solution class
    Added a method to summarize ranges from a list of integers.

--- day6.py
+++ day6.py
@@ -13,3 +13,30 @@ class Solution:
             return True
         return self.isHappy(sum, curr)
         
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        res = []
+        if(len(nums)==0):
+            return []
+        currS = str(nums[0])
+        start = nums[0]
+        for i in range(1, len(nums)):
+            if(nums[i]-nums[i-1] != 1 and nums[i-1]!=start):
+                currS+="->"
+                currS+=str(nums[i-1])
+                res.append(currS)
+                currS = str(nums[i])
+                start = nums[i]
+            elif(nums[i]-nums[i-1] != 1 and nums[i-1]==start):
+                res.append(currS)
+                currS = str(nums[i])
+                start = nums[i]
+        if(start == nums[len(nums)-1]):
+                res.append(currS)
+        else:
+            currS+="->"
+            currS+=str(nums[len(nums)-1])
+            res.append(currS)
+        return res
+
+            
