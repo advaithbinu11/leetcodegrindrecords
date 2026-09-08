333c978ab4d7440b228be078e4a4bd66d52d4ea3
Advaith Binu <71909050+advaithbinu11@users.noreply.github.com>
2026-09-07 21:33:27 -0500

    Optimize jump game solution with greedy approach
    Refactored jump game solution to use an optimized greedy algorithm.

--- week2/day9.py
+++ week2/day9.py
@@ -11,3 +11,11 @@ class Solution:
                         jumps[i] = True
             i-=1
         return jumps[0]
+#Optimized Greedy Algorithm
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums)-1
+        for i in range(len(nums)-2, -1, -1):
+            if(i+nums[i]>=goal):
+                goal = i
+        return goal == 0
